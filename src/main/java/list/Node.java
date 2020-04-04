80657492b430cc6f98c96c9930d911e41ce23908
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-04-04 23:59:27 +0200

    lombok removal

--- src/main/java/list/Node.java
+++ src/main/java/list/Node.java
@@ -1,14 +1,15 @@
 package list;
 
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
 public class Node<K, V> {
+
     public K key;
     public V val;
     public int frequency;
     public Node<K, V> prev;
     public Node<K, V> next;
+
+    public Node() {
+    }
 
     public Node(K key, V val) {
         this.key = key;
