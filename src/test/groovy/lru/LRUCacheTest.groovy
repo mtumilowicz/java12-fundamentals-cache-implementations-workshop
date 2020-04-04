545c040a08b283ae7652a85a856d0bb2d825304a
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-04-04 22:06:10 +0200

    LRUCacheTest.groovy tests

--- src/test/groovy/lru/LRUCacheTest.groovy
+++ src/test/groovy/lru/LRUCacheTest.groovy
@@ -5,9 +5,38 @@ import spock.lang.Specification
 class LRUCacheTest extends Specification {
     def 'empty'() {
         given:
-        def empty = new LRUCache(1)
+        def empty = new LRUCache<Integer, Integer>(1)
 
         expect:
         !empty.get(1)
+    }
+
+    def 'single value'() {
+        given:
+        def single = new LRUCache<Integer, Integer>(1)
+
+        when:
+        single.put(1, 2)
+
+        then:
+        single.get(1) == 2
+    }
+
+    def 'get refreshes recently used'() {
+        given:
+        def single = new LRUCache<Integer, Integer>(2)
+
+        and:
+        single.put(1, 1)
+        single.put(2, 2)
+
+        when:
+        single.get(1)
+
+        and:
+        single.put(3, 3)
+
+        then:
+        !single.get(2)
     }
 }
