293b2c5576381bc4e2c1301183c53901c31e1ea2
mtumilowicz <michal.tumilowicz01@gmail.com>
2020-04-05 11:34:58 +0200

    lru tests

--- src/test/groovy/lru/LRUCacheTest.groovy
+++ src/test/groovy/lru/LRUCacheTest.groovy
@@ -23,7 +23,7 @@ class LRUCacheTest extends Specification {
         cache.get(1) == 2
     }
 
-    def 'get moves entry as a least recently used'() {
+    def 'get marks entry as a most recently used'() {
         given: 'empty cache with threshold 2'
         def cache = new LRUCache<Integer, Integer>(2)
 
@@ -61,6 +61,30 @@ class LRUCacheTest extends Specification {
 
         and: 'newest entries are present'
         cache.get(3) == 3
+        cache.get(4) == 4
+        cache.get(5) == 5
+    }
+
+    def 'changing value of existing key marks entry as a most recently used'() {
+        given: 'empty cache with threshold 3'
+        def cache = new LRUCache(3)
+
+        when: 'overflow threshold by 2'
+        cache.put(1, 1)
+        cache.put(2, 2)
+
+        and: 'replace value to 3'
+        cache.put(1, 3)
+
+        and: 'put other values'
+        cache.put(4, 4)
+        cache.put(5, 5)
+
+        then: 'oldest entries are removed'
+        !cache.get(2)
+
+        and: 'newest entries are present'
+        cache.get(1) == 3
         cache.get(4) == 4
         cache.get(5) == 5
     }
